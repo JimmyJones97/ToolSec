254d6df73b28f6d7ef71b27069a21e4d9d13fc8c
binaryslark <binaryslark@gmail.com>
2021-06-29 00:49:19 +0800

    fix fifo error

--- fifo_reader/src/FIFOReader.cpp
+++ fifo_reader/src/FIFOReader.cpp
@@ -16,11 +16,18 @@ FIFOReader::FIFOReader(const char *path){
 int FIFOReader::init_fifo(){
     int fd;
 
+    if(mkfifo(m_path, O_CREAT | O_EXCL) < 0 && (errno != EEXIST)){
+        printf("cannot create fifo file\n");
+        return -1;
+    }
+    printf("mkfifo success:%s\n", m_path);
+
     fd = open(m_path, O_RDONLY | O_NONBLOCK);
     if(fd == -1){
         printf("open error:%s\n", strerror(errno));
         return -1;
     }
+    m_fd = fd;
     return 0;
 }
 
@@ -28,12 +35,23 @@ int FIFOReader::read_data(){
     char buffer[BUF_SIZE];
     int bytes_read = -1;
 
-    memset(buffer, 0, BUF_SIZE);
-    bytes_read = read(m_fd, buffer, BUF_SIZE);
-    if(bytes_read == -1){
-        printf("read error:%s\n", strerror(errno));
+    
+
+    FILE *fp = fdopen(m_fd, "r");
+    if(!fp){
+        printf("fdopen failed:%s\n", strerror(errno));
         return -1;
     }
+    memset(buffer, 0, BUF_SIZE);
+    fgets(buffer, BUF_SIZE, fp);
+    bytes_read = strlen(buffer);
+    printf("bytes_read:%d\n", bytes_read);
+    //bytes_read = read(m_fd, buffer, BUF_SIZE);
+    //printf("read return %d\n", bytes_read);
+    // if(bytes_read == -1){
+    //     printf("read error:%s\n", strerror(errno));
+    //     return -1;
+    // }
     printf("data:%s\n", buffer);
     
     return 0;    
